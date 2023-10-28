6918966f6ac63d770ceb92036dddb49f241bd8ef
acipsuhendar <acipalmusawwa@gmail.com>
2023-10-28 12:22:20 +0700

    perubahan lagi

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import MapView from "../views/MapView.vue";
+import PerangkatView from "../views/PerangkatView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
